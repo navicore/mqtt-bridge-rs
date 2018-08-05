02032e3a0ab59ae0bf64a1ece159edb4e605bbe6
Ed Sweeney <ed@onextent.com>
2018-08-04 22:21:32 -0700

    refactor

--- src/main.rs
+++ src/main.rs
@@ -13,29 +13,24 @@ use std::sync::Arc;
 use std::time;
 mod settings;
 
-fn new_client(mqtt_settings: settings::Mqtt) -> mqttc::Client {
-    let url = mqtt_settings.url;
-    let username = mqtt_settings.username;
-    let password = mqtt_settings.password;
-    let use_tls = mqtt_settings.tls;
+fn new_client(cfg: settings::Mqtt) -> mqttc::Client {
     use mqttc::{ClientOptions, ReconnectMethod};
     use netopt::{NetworkOptions, SslContext};
 
     // Using ssl network connection
     let mut netopt = NetworkOptions::new();
-    if use_tls {
-        let ssl = SslContext::default();
-        netopt.tls(ssl);
+    if cfg.tls {
+        netopt.tls(SslContext::default());
     }
 
     // Using credentials for client
     let mut opts = ClientOptions::new();
-    opts.set_username(username.to_string());
-    opts.set_password(password.to_string());
+    opts.set_username(cfg.username.to_string());
+    opts.set_password(cfg.password.to_string());
     let timeout = time::Duration::from_secs(1_0000);
     opts.set_reconnect(ReconnectMethod::ReconnectAfter(timeout));
 
-    let x = opts.connect(url, netopt).unwrap();
+    let x = opts.connect(cfg.url, netopt).unwrap();
     x
 }
 
@@ -43,8 +38,7 @@ fn main() {
     let settings = Settings::new().unwrap();
     let mut in_client = new_client(settings.in_client);
 
-    let in_topic = settings.in_topic;
-    in_client.subscribe(in_topic.as_str()).ok();
+    in_client.subscribe(settings.in_topic.as_str()).ok();
     let mut out_client = new_client(settings.out_client);
 
     loop {
@@ -54,9 +48,8 @@ fn main() {
                 match Arc::try_unwrap(message.payload) {
                     Ok(text) => {
                         let msg = str::from_utf8(&text).unwrap();
-                        let out_topic = settings.out_topic.clone();
                         out_client
-                            .publish(out_topic, msg, PubOpt::at_least_once())
+                            .publish(settings.out_topic.clone(), msg, PubOpt::at_least_once())
                             .ok();
                     }
                     Err(err) => {
